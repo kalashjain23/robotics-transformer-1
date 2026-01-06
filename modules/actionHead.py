92c7ea9cf94b040aad7e7468a0a5ffbf74ac1b44
Kalash Jain <kalashjain513@gmail.com>
2026-01-07 00:54:59 +0530

    feat: add action head module

---
+++ modules/actionHead.py
@@ -0,0 +1,33 @@
+from torch import nn, Module, Tensor
+
+
+class ActionHead(Module):
+    def __init__(
+        self, 
+        input_dim: int = 512, 
+        output_dim: int = 256, 
+        num_actions: int = 11,
+        num_images: int = 6,
+        tokens_per_image: int = 8
+    ):
+        super().__init__()
+        
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.num_actions = num_actions
+        self.num_images = num_images
+        self.tokens_per_image = tokens_per_image
+        
+        self.head = nn.Linear(input_dim, num_actions * output_dim)
+        
+    def forward(self, x: Tensor):
+        B = x.shape[0]
+        x = x.reshape(B, self.num_images, self.tokens_per_image, self.input_dim)
+        
+        # last token of each image
+        x = x[:, :, -1, :]
+        
+        logits = self.head(x)
+        logits = logits.reshape(B, self.num_images, self.num_actions, self.output_dim)
+        
+        return logits
